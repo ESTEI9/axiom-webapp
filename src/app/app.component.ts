09e4dc5e546d6c47b1527fbed216f6f606f9b4c5
Eric Steinecke <eric.steinecke@gmail.com>
2020-02-04 08:40:37 -0500

    init for iOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+declare var cordova: any;
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -9,18 +10,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  public appPages = [
-    // {
-    //   title: 'Home',
-    //   url: '/home',
-    //   icon: 'home'
-    // },
-  ];
+export class AppComponent  {
+
+  private loading = true;
 
   constructor(
     private platform: Platform,
-    private statusBar: StatusBar
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private toastCtrl: ToastController
   ) {
     this.initializeApp();
   }
@@ -28,6 +26,46 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.loadSite().then((resp: any) => {
+        this.splashScreen.hide();
+        this.loading = false;
+        if (resp !== true) {
+          this.errorToast();
+        }
+      });
     });
+  }
+
+  loadSite() {
+    this.loading = true;
+    return new Promise((resolve, reject) => {
+      try {
+        const browserOptions = 'clearcache=yes,location=no,zoom=no,usewkwebview=yes';
+        const browser = cordova.InAppBrowser.open('http://exchange.unyti.org/', '_blank', browserOptions);
+        browser.show();
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  reload() {
+    this.loading = true;
+    this.loadSite().then((resp: any) => {
+      this.loading = false;
+      if (resp !== true) {
+        this.errorToast();
+      }
+    });
+  }
+
+  async errorToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'There was a problem loading. Please restart or try again later.',
+      duration: 3000,
+      color: 'error'
+    });
+    await toast.present();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,14 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
-    InAppBrowser,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
